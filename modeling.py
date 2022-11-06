33688eb3804eeac6c756b648c502d0180d209e2c
dew54 <dvdvdm96@gmail.com>
2022-11-06 10:32:05 +0100

    edit

--- modeling.py
+++ modeling.py
@@ -2,6 +2,7 @@ import numpy as np
 import random as rm
 import math
 import pandas as pd
+from scipy.stats import uniform
 from matplotlib import pyplot as plt
 from getStats import getStats
 from statsmodels.distributions.empirical_distribution import ECDF
@@ -16,7 +17,7 @@ def weatherGame(T):
     totalRain = []
     averageRain = 0
     X = []
-    for i in range(1, T):
+    for i in range(0, T):
         u = np.random.uniform()
         if u <= pc:
             rainAmount = np.random.exponential(scale=mu[i], size=None)
@@ -36,6 +37,7 @@ def weatherGame(T):
 
     for rain in totalRain:
         averageRain += rain/T
+    return totalRain
     # print("Possible states: " + str(weatherList))
     # print("average rain: " +  str(averageRain))
 
@@ -45,20 +47,6 @@ def computeAR1(previousVal):
     Bprime = B.dot(epsilon)
     zed = Aprime + Bprime
     return zed
-
-
-
-
-
-def computeAR():
-    zeta = np.array([[0, 0, 0], ]*T)
-    for t in range(1, T):
-        epsilon = np.array([np.random.normal(0,1,1)[0], np.random.normal(0,1,1)[0], np.random.normal(0,1,1)[0]] )
-        Aprime = A.dot(zeta[t-1])
-        Bprime = B.dot(epsilon)
-        zed = Aprime + Bprime
-        zeta = np.append(zeta, [zed], axis=0)
-    return zeta
 
 
 def computeNonPrecip(t, isWet):
@@ -107,23 +95,36 @@ def computeNonPrecip(t, isWet):
     
 
 def earthQwakeGame():
-    a = -100
+    a = 10
     b = 1
+    y = []
+    # probVector = []
 
-
-    for m in range(0, 1300):
-        print(m)
-        print(10 **((a-b)*(m/100)))
-        y.append(10 **((a-b)*(m/100)))
-    EQ_cdf = ECDF(y)
-
-    # print(EQ_cdf.y)
-    print(y)
-
+    for m in range(0, 13):
+        
+        N = 10 **((a-b*(m)))
+        print(N)
+        y.append(N)
+        
+    probVector=([ i/sum(yps for yps in y) for i in y])
+    cdf = ECDF([i/sum(yps for yps in y) for i in y])
+    cdf.x[0] = 0
+    print(cdf.x)
+    
+    print(np.array(cdf.x).sum())
+    # for x in cdf.x:
+        # print(x) 
+    eq_cdf = cdf.x
     for i in range(0, T):
-        EQ_samples.append(sampleGivenCDF(EQ_cdf.y)/100)
-
-
+        u = np.random.uniform()
+        if u <= EQ_p:
+            sample = discrete_inverse_trans(probVector)
+            print(sample)
+            EQ_samples.append(sample)
+        else: 
+            EQ_samples.append(0)
+    return probVector
+    
 
 
 def hydroGame():
@@ -134,14 +135,24 @@ def popGame():
     pass
 
 
+def discrete_inverse_trans(prob_vec):
+    U=uniform.rvs(size=1)
+    if U<=prob_vec[0]:
+        return 1
+    else:
+        for i in range(1,len(prob_vec)+1):
+            if sum(prob_vec[0:i])<U and sum(prob_vec[0:i+1])>U:
+                return i+1
+
+
 
 def sampleGivenCDF(y):
     u = np.random.uniform()
-    if u < y[1]:
+    if u < y[0]:
         return 1
     else :    
         for idx in range(y.size):
-            if(y[idx] < u and y [idx+1] > u):
+            if(y[idx] < u and y[idx+1] > u):
                 return idx
             
             
@@ -152,140 +163,141 @@ if __name__ == "__main__":
 # The statespace
     global zed
 
-    states = ["dry","wet"]
-    T = 365 # 1 month 
-    # Possible sequences of events
-    transitionName = [["DD","DW"],["WD","WW"]]
-    
-    p01 = 0.4 # wet day after dry day
-    p00 = 1 - p01 # (dry day following a dry day
-    p11 = 0.6 # wet day after a wet day  
-    p10 = 1 - p11       
-    
-    transitionMatrix = [[p00,p01],[p10,p11]]
+states = ["dry","wet"]
+T = 365*2 # 1 month 
+# Possible sequences of events
+transitionName = [["DD","DW"],["WD","WW"]]
 
-    pi = p01/(1 + p01 - p11)
-    r = p11 - p01
-    average = pi * T                            # average number of wet days
-    variance = pi * (1-pi) * T * (1 + r)/(1-r)  # Variance of the number of wet days from average
+p01 = 0.4 # wet day after dry day
+p00 = 1 - p01 # (dry day following a dry day
+p11 = 0.6 # wet day after a wet day  
+p10 = 1 - p11       
 
+transitionMatrix = [[p00,p01],[p10,p11]]
 
-    # if sum(transitionMatrix[0])+sum(transitionMatrix[1]) != 2:
-    #     print("Somewhere, something went wrong. Transition matrix, perhaps?")
-    # else: print("All is gonna be okay, you should move on!! ;)")
-    
-    
-    stats, df = getStats()
-
-    #mu = 120#stats['E_rain']                                      #  mm per day 
-    A = np.array([[0.567, 0.086, -0.002], [0.253, 0.504, -0.050], [-0.006, 0.039, 0.244]])
-    B = np.array([[0.718, 0, 0], [0.328, 0.637, 0], [0.238, -0.314, 0.873]])
+pi = p01/(1 + p01 - p11)
+r = p11 - p01
+average = pi * T                            # average number of wet days
+variance = pi * (1-pi) * T * (1 + r)/(1-r)  # Variance of the number of wet days from average
 
 
-    E_tmin0 = 2#stats['E_tmin_0']
-    E_tmax0 = 15#stats['E_tmax_0']
-    E_r0 = stats['E_r_0']
-
-    S_tmin0 = 7#stats['VAR_tmin_0']
-    S_tmax0 = 5#stats['VAR_tmax_0']
-    S_r0 = stats['VAR_r_0']
-
-    #Override
-    # S_r0 = S_r0/10
-
-    E_tmin1 = 0#stats['E_tmin_1']
-    E_tmax1 = 12#stats['E_tmax_1']
-    E_r1 = stats['E_r_1']
-
-    S_tmin1 = 6#stats['VAR_tmin_1']
-    S_tmax1 = 5#stats['VAR_tmax_1']
-    S_r1 = stats['VAR_r_1']
-    # S_r1 = S_r1/10
-
-    initialValue = np.array([0, 0, 0])
-    zed = initialValue
-    
+# if sum(transitionMatrix[0])+sum(transitionMatrix[1]) != 2:
+#     print("Somewhere, something went wrong. Transition matrix, perhaps?")
+# else: print("All is gonna be okay, you should move on!! ;)")
 
 
-    C_r0 = -20000
-    C_r1 = -900
-    C_tmin0 = -5
-    C_tmax0 = -6
-    C_tmin1 = -3
-    C_tmax1 = -4
-    wave_tmin0 = []
-    wave_tmax0 = []
-    wave_r0 = []
-    wave_tmin1 = []
-    wave_tmax1 = []
-    wave_r1 = []
-    mu = []
+stats, df = getStats()
+
+#mu = 120#stats['E_rain']                                      #  mm per day 
+A = np.array([[0.567, 0.086, -0.002], [0.253, 0.504, -0.050], [-0.006, 0.039, 0.244]])
+B = np.array([[0.718, 0, 0], [0.328, 0.637, 0], [0.238, -0.314, 0.873]])
 
 
-    y = []
-    EQ_cdf = []
-    EQ_samples = []
-    
+E_tmin0 = 2#stats['E_tmin_0']
+E_tmax0 = 15#stats['E_tmax_0']
+E_r0 = stats['E_r_0']
 
+S_tmin0 = 7#stats['VAR_tmin_0']
+S_tmax0 = 5#stats['VAR_tmax_0']
+S_r0 = stats['VAR_r_0']
 
-    for i in range(T):
+#Override
+# S_r0 = S_r0/10
 
-        wave_tmin0.append(E_tmin0 + C_tmin0 * (np.cos(0.0172*(i - T - 30))))
-        wave_tmax0.append(E_tmax0 + C_tmax0 * (np.cos(0.0172*(i - T -30))))
-        wave_r0.append(E_r0 + C_r0 * (np.cos(0.0172*(i - T -30))))
+E_tmin1 = 0#stats['E_tmin_1']
+E_tmax1 = 12#stats['E_tmax_1']
+E_r1 = stats['E_r_1']
 
-        wave_tmin1.append(E_tmin1 + C_tmin1 * (np.cos(0.0172*(i - T -30))))
-        wave_tmax1.append(E_tmax1 + C_tmax1 * (np.cos(0.0172*(i - T-30))))
-        wave_r1.append(E_r1 + C_r1 * (np.cos(0.0172*(i - T-30))))
-        mu.append(80 + 20*np.cos(0.0172*(i - 2*T -30)))
+S_tmin1 = 6#stats['VAR_tmin_1']
+S_tmax1 = 5#stats['VAR_tmax_1']
+S_r1 = stats['VAR_r_1']
+# S_r1 = S_r1/10
+
+initialValue = np.array([0, 0, 0])
+zed = initialValue
 
 
 
-    # print(stats)
-
-    
-
-
-    t_min = [0]
-    t_max = [10]
-    r = [6000]
-    temp_diff = [10]
-
-    z = [[0,0,0]]
-
-
-    weatherGame(T)
-    earthQwakeGame()
+C_r0 = -20000
+C_r1 = -900
+C_tmin0 = -5
+C_tmax0 = -6
+C_tmin1 = -3
+C_tmax1 = -4
+wave_tmin0 = []
+wave_tmax0 = []
+wave_r0 = []
+wave_tmin1 = []
+wave_tmax1 = []
+wave_r1 = []
+mu = []
 
 
+EQ_prob_vec = []
 
-    # print('Average is: ', average)
-    # print('Average percent is: ', average * 100/T)
-    # print('Variance is: ', variance)
-    # # weatherGame(T)
-    X = range(0, T)
-
-    
-    fig1, axs1 = plt.subplots(3, 2)
-
-    axs1[0][0].plot(X, df['rain'][0:T] )
-    axs1[0][0].set_title('Rainy days')
-    #plt.grid()
-
-    axs1[1][0].plot(X, t_min, t_max)
-    axs1[1][0].set_title('t min')
-    
-    # axs1[1][0].plot(X, numNodes)
-    # axs1[1][0].set_title('# of nodes')
-
-    axs1[0][1].plot(X,  EQ_samples )
-    axs1[0][1].set_title('scosse')
-
-    axs1[1][1].plot(X,  r )
-    axs1[1][1].set_title('radiation')
+eq_cdf = []
+EQ_samples = []
+EQ_p = 0.01
+EQ_mu = 3
 
 
-    axs1[2][1].plot([x/100 for x in range(1300)],  y )
-    axs1[2][1].set_title('AutoRegressivo')
 
-    plt.show()
+for i in range(T):
+
+    wave_tmin0.append(E_tmin0 + C_tmin0 * (np.cos(0.0172*(i - T - 30))))
+    wave_tmax0.append(E_tmax0 + C_tmax0 * (np.cos(0.0172*(i - T -30))))
+    wave_r0.append(E_r0 + C_r0 * (np.cos(0.0172*(i - T -30))))
+
+    wave_tmin1.append(E_tmin1 + C_tmin1 * (np.cos(0.0172*(i - T -30))))
+    wave_tmax1.append(E_tmax1 + C_tmax1 * (np.cos(0.0172*(i - T-30))))
+    wave_r1.append(E_r1 + C_r1 * (np.cos(0.0172*(i - T-30))))
+    mu.append(80 + 20*np.cos(0.0172*(i - 2*T -30)))
+
+
+
+# print(stats)
+
+
+
+
+t_min = [0]
+t_max = [10]
+r = [6000]
+temp_diff = [10]
+
+z = [[0,0,0]]
+
+
+totalRain = weatherGame(T)
+EQ_prob_vec = earthQwakeGame()
+
+X = range(0, T)
+
+
+fig1, axs1 = plt.subplots(3, 2)
+
+axs1[0][0].plot(X, totalRain[0:T] )
+axs1[0][0].set_title('Rainy days')
+#plt.grid()
+
+axs1[1][0].plot(X, t_min, t_max)
+axs1[1][0].set_title('t min')
+
+# axs1[1][0].plot(X, numNodes)
+# axs1[1][0].set_title('# of nodes')
+
+axs1[0][1].plot(X,  EQ_samples )
+axs1[0][1].set_title('scosse')
+
+axs1[1][1].plot(X,  r )
+axs1[1][1].set_title('radiation')
+
+
+# axs1[2][1].plot([x for x in range(729)],  z )
+# axs1[2][1].set_title('AutoRegressivo')
+
+# axs1[2][0].plot([x for x in range(14)],  eq_cdf )
+# axs1[2][0].set_title('AutoRegressivo')
+
+
+plt.show()
--- testing.py
+++ testing.py
@@ -1,16 +1,33 @@
+import numpy as np
+import random as rm
+import math
 import pandas as pd
+from scipy.stats import uniform
+from matplotlib import pyplot as plt
+from getStats import getStats
+from statsmodels.distributions.empirical_distribution import ECDF
 
 
-df = pd.read_csv('tarvisio-2021.csv')
-daily_df = pd.read_csv('orari-Tarvisio-2021.csv')
-daily_df.fillna(0)
-df_wet = (df[df['rain'] > 0] ) 
-df_dry = (df[df['rain'] == 0] ) 
-for m in range(1, 13):
-    mese = daily_df[daily_df['mese'] == m]
-    print(int(mese['index'].size/24))
-    for g in range(int(mese['index'].size/24)):
-        print(mese[mese['giorno'] == g]['radiaz'].mean() )
-        
 
+def discrete_inverse_trans(prob_vec):
+    U=uniform.rvs(size=1)
+    if U<=prob_vec[0]:
+        return 1
+    else:
+        for i in range(1,len(prob_vec)+1):
+            if sum(prob_vec[0:i])<U and sum(prob_vec[0:i+1])>U:
+                return i+1
+
+
+
+a = 20
+b = 2
+y = []
+
+
+for m in range(0, 13):
     
+    y.append(10 **((a-b*(m))))
+cdf = ECDF([i/sum(yps for yps in y) for i in y])
+for x in cdf.x:
+    print(x) 
